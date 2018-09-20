acacd10d6089252e0bab9a5ea5ce01af8c8fd278
zhouwei <zhouwei@xprinciple.com>
2018-09-20 20:04:24 +0800

    generated by xcode

---
+++ app/app/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  app
+//
+//  Created by zhouwei on 2018/9/20.
+//  Copyright © 2018年 ibetame. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
